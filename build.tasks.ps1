15a79088235308d46d4767ebcdcda0c32163ab6b
Mark Clearwater <mark.clearwater@xero.com>
2014-12-08 18:08:23 +1300

    Added initial solution with psake build script

---
+++ build.tasks.ps1
@@ -0,0 +1,17 @@
+properties {
+	# build variables
+	$framework = "4.5.1"		# .net framework version
+	$configuration = "Release"	# build configuration
+	
+	# directories
+	$base_dir = . resolve-path .\
+	
+	# files
+	$sln_file = "$base_dir\src\csmacnz.Coveralls.sln"
+}
+
+task default
+
+task build {
+	exec { msbuild "/t:Clean;Build" "/p:Configuration=$configuration" $sln_file }
+}
---
+++ src/csmacnz.Coveralls/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csmacnz.Coveralls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
