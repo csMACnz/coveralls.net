fadebc4e16a4633378a446c84efc03f1eb7cdaff
Mark Clearwater <mclearwater@gmail.com>
2014-12-30 17:48:20 +1300

    trying to fix pscx loading scripts

--- Build.environment.ps1
+++ Build.environment.ps1
@@ -1,2 +1,16 @@
 choco install psake
 choco install pscx
+
+$pscxPath = "C:\Program Files (x86)\PowerShell Community Extensions\Pscx3\Pscx";
+
+if (-not (Test-Path $pscxPath))
+{
+    $pscxPath = $null;
+    Write-Host "Searching for the pscx powershell module.";
+    $pscxPath = (Get-ChildItem -Path "C:\Program Files\" -Filter "pscx.dll" -Recurse).FullName;
+    if (!$pscxPath) { $pscxPath = (Get-ChildItem -Path "C:\Program Files (x86)\" -Filter "pscx.dll" -Recurse).FullName; }
+    $pscxPath = Split-Path $pscxPath;
+    Write-Host "Found it at " + $pscxPath;
+}
+
+$env:PSModulePath = $env:PSModulePath + ";" + $pscxPath;
