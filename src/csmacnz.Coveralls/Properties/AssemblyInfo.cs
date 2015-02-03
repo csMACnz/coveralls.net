1dca53f970904029baac484a487e0b5343a0fd50
Mark Clearwater <csmacnz@csmac.co.nz>
2015-02-03 22:19:00 +1300

    Updated versioning to 0.2

--- src/csmacnz.Coveralls/Properties/AssemblyInfo.cs
+++ src/csmacnz.Coveralls/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("csmacnz")]
 [assembly: AssemblyProduct("csmacnz.Coveralls")]
-[assembly: AssemblyCopyright("Copyright © csmacnz 2014")]
+[assembly: AssemblyCopyright("Copyright © csmacnz 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0.0")]
