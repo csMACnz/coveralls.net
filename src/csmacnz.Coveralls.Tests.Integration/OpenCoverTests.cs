32bfffc8418642948b82c39389c6ba5f6df80b6c
Mark Clearwater <csmacnz@csmac.co.nz>
2016-08-19 23:37:52 +0100

    Added --multiple option

--- src/csmacnz.Coveralls.Tests.Integration/OpenCoverTests.cs
+++ src/csmacnz.Coveralls.Tests.Integration/OpenCoverTests.cs
@@ -19,7 +19,35 @@ namespace csmacnz.Coveralls.Tests.Integration
         }
 
         [Fact]
+        public void EmptyReport_MultipleMode_RunsSuccessfully()
+        {
+            var emptyFilePath = Path.Combine(RepositoryPaths.GetSamplesPath(), "opencover", "Sample1", "EmptyReport.xml");
+            var results = DryRunCoverallsMultiModeWithInputFile(emptyFilePath);
+
+            Assert.Equal(0, results.ExitCode);
+        }
+
+        [Fact]
         public void ReportWithOneFile_RunsSuccessfully()
+        {
+            var coverageFilePath = BuildReportWithOneFile();
+
+            var results = DryRunCoverallsWithInputFile(coverageFilePath);
+
+            Assert.Equal(0, results.ExitCode);
+        }
+
+        [Fact]
+        public void ReportWithOneFile_MultipleMode_RunsSuccessfully()
+        {
+            var coverageFilePath = BuildReportWithOneFile();
+
+            var results = DryRunCoverallsMultiModeWithInputFile(coverageFilePath);
+
+            Assert.Equal(0, results.ExitCode);
+        }
+
+        private static string BuildReportWithOneFile()
         {
             string sampleFolderPath = Path.Combine(RepositoryPaths.GetSamplesPath(), "opencover", "Sample2");
             var sampleCoverageFile = Path.Combine(sampleFolderPath, "SingleFileReport.xml");
@@ -28,18 +56,22 @@ namespace csmacnz.Coveralls.Tests.Integration
             var classFilePath = TestFolders.GetTempFilePath(Guid.NewGuid() + ".cs");
             File.Copy(sampleClassFile, classFilePath);
             var doc = XDocument.Load(sampleCoverageFile);
-            var classFile = doc.XPathSelectElements("//CoverageSession/Modules/Module/Files/File").FirstOrDefault(e => e.Attribute("fullPath").Value.EndsWith("Class1.cs"));
+            var classFile =
+                doc.XPathSelectElements("//CoverageSession/Modules/Module/Files/File")
+                    .FirstOrDefault(e => e.Attribute("fullPath").Value.EndsWith("Class1.cs"));
             classFile.Attribute("fullPath").SetValue(classFilePath);
             doc.Save(coverageFilePath);
-
-            var results = DryRunCoverallsWithInputFile(coverageFilePath);
-
-            Assert.Equal(0, results.ExitCode);
+            return coverageFilePath;
         }
 
         private static CoverageRunResults DryRunCoverallsWithInputFile(string inputFilePath)
         {
             return CoverallsTestRunner.RunCoveralls(string.Format("--opencover -i {0} --dryrun --repoToken MYTESTREPOTOKEN", inputFilePath));
+        }
+
+        private static CoverageRunResults DryRunCoverallsMultiModeWithInputFile(string inputFilePath)
+        {
+            return CoverallsTestRunner.RunCoveralls(string.Format("--multiple -i opencover={0} --dryrun --repoToken MYTESTREPOTOKEN", inputFilePath));
         }
     }
 }
