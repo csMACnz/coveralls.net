f0fd1cd143cd40a772eecbbdef35966d5e72e47e
Mark Clearwater <csmacnz@csmac.co.nz>
2017-04-15 19:10:36 +0100

    loose parsing scaffolding added with 0 coverage data so far

--- src/csmacnz.Coveralls/Parsers/ReportGeneratorParser.cs
+++ src/csmacnz.Coveralls/Parsers/ReportGeneratorParser.cs
@@ -19,8 +19,35 @@ namespace csmacnz.Coveralls.Parsers
             var sourceFiles = new List<CoverageFile>();
             foreach (var fileName in documents.Keys.Where(k => k != "Summary.xml"))
             {
-            }
+                var rootDocument = documents[fileName];
+                var filesElement = rootDocument.Root?.Element("Files");
+                if (filesElement != null)
+                {
 
+                    foreach (var fileElement in filesElement.Elements("File"))
+                    {
+                        var filePath = fileElement.Attribute("name").Value;
+                        var source = new List<string>();
+                        var coverage = new List<int?>();
+                        foreach (var lineAnalysis in fileElement.Elements("LineAnalysis"))
+                        {
+                            var line = lineAnalysis.Attribute("line").Value;
+                            var visits = lineAnalysis.Attribute("visits").Value;
+                            var coverageMode = lineAnalysis.Attribute("coverage").Value;
+                            var coveredbranches = lineAnalysis.Attribute("coveredbranches").Value;
+                            var totalbranches = lineAnalysis.Attribute("totalbranches").Value;
+                            var content = lineAnalysis.Attribute("content").Value;
+
+                            source.Add(content);
+                            coverage.Add(null);
+                        }
+
+                        var sourceDigest = Crypto.CalculateMD5Digest(string.Join(",", source.ToArray()));
+                        var coverageFile = new CoverageFile(filePath, sourceDigest, coverage.ToArray());
+                        sourceFiles.Add(coverageFile);
+                    }
+                }
+            }
             return sourceFiles;
         }
     }
