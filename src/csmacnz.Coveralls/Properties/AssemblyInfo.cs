fd18767c4d23ff2b0b54295654270dae7c8cdc17
Mark Clearwater <csmacnz@csmac.co.nz>
2017-11-15 09:53:22 +0000

    Add rules, with intial disabling until fixed.

--- src/csmacnz.Coveralls/Properties/AssemblyInfo.cs
+++ src/csmacnz.Coveralls/Properties/AssemblyInfo.cs
@@ -1,16 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("csmacnz.Coveralls")]
-[assembly: AssemblyDescription("Coveralls.io uploader for .NET Code Coverage")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("csMACnz")]
-[assembly: AssemblyProduct("csmacnz.Coveralls")]
-[assembly: AssemblyCopyright("Copyright © csMACnz 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("509f0e5e-6adb-4564-99fb-4db063585bef")]
 [assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("0.0.1.0")]
 [assembly: AssemblyInformationalVersion("0.0.1.0-gamma")]
